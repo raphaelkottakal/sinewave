f839673a1525cb5092c8ef412b35fc68b0e0b781
Raphael Kottakal <raphaelsemail@gmail.com>
2018-07-31 21:13:30 +0530

    completed

--- sketch.js
+++ sketch.js
@@ -1,27 +1,80 @@
 var offset = 0;
+var waves = [];
 var firstSinewave;
+var tapReleased = true;
+var tapStart;
+var ghostOffet = 0;
+var ghostWidth = 0;
+var ghostSinewave;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  colorMode(HSL, 360, 100, 100, 1);
   firstSinewave = new Sinewave({
     x: (windowWidth / 2) - 100,
     y: windowHeight / 2,
-    length: 200,
-    angle: 0,
-    color: 255,
-    weight: 3,
-    // showPoints: true,
-    // segments: 5
+    length: 200
   });
+  waves.push(firstSinewave);
 }
 
 function draw() {
   background(0);
-  firstSinewave.display();
+  for (var i = 0; i < waves.length; i++) {
+    waves[i].display();
+  }
+  if (mouseIsPressed && tapStart) {
+    drawGhostLine(tapStart);
+  }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function touchStarted() {
+  if (!tapReleased) {
+    return;
+  }
+  tapReleased = false;
+  ghostSinewave = null;
+  ghostOffet = 0;
+  ghostWidth = 0;
+  tapStart = createVector(mouseX, mouseY);
+}
+
+function touchMoved(e) {
+  e.preventDefault();
+}
+
+function touchEnded() {
+  if (ghostSinewave.mag() > 20) {
+    if (waves.length === 30) {
+      waves.shift();
+    }
+    var newWave = new Sinewave({
+      x: tapStart.x,
+      y: tapStart.y,
+      angle: ghostSinewave.mult(-1).heading(),
+      length: ghostSinewave.mag(),
+      weight: ghostWidth
+    });
+    waves.push(newWave);
+  }
+  tapStart = null;
+  tapReleased = true;
+  return false;
+}
+
+function drawGhostLine(start) {
+  const currentPoint = createVector(mouseX, mouseY);
+  const currentVector = p5.Vector.sub(start, currentPoint);
+  stroke(100, 0.5);
+  ghostWidth = abs((cos(ghostOffet) * 6)) + 2;
+  strokeWeight(ghostWidth);
+  line(start.x, start.y, currentPoint.x, currentPoint.y);
+  ghostOffet += 0.05;
+  ghostSinewave = currentVector;
 }
 
 function Sinewave(opts) {
